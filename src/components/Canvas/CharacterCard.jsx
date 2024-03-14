c0770084a264ed90b6f7b9544773d8a415306526
Natthapong Pecpipat <102534597+XspOttEr15@users.noreply.github.com>
2024-03-14 18:47:21 +0700

    change a lot 20

--- src/components/Canvas/CharacterCard.jsx
+++ src/components/Canvas/CharacterCard.jsx
@@ -47,12 +47,15 @@ export function CharacterCardThree() {
 
   export function Characterdisplay() {
     return (
-      <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
-        <mesh>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
-      </Canvas>
+      // <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
+      //   <mesh>
+      //     <boxGeometry />
+      //     <meshNormalMaterial />
+      //   </mesh>
+      // </Canvas>
+      <div className=" lg:w-full md:w-full lg:max-h-[500px] lg:h-auto md:h-[350px] overflow-hidden ring-4 ring-emerald-400  rounded-xl">
+        <img src="/images/Characters/Lunar/lunar.png" className=" w-full h-auto" />
+      </div>
     );
 
   }
@@ -60,34 +63,43 @@ export function CharacterCardThree() {
 
   export function Characterdisplaytwo() {
       return (
-        <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
-          <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-          </mesh>
-        </Canvas>
+        // <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
+        //   <mesh>
+        //     <boxGeometry />
+        //     <meshNormalMaterial />
+        //   </mesh>
+        // </Canvas>
+        <div className=" lg:w-full md:w-full lg:max-h-[500px] lg:h-auto md:h-[350px] overflow-hidden ring-4 ring-emerald-400  rounded-xl">
+        <img src="/images/Characters/Rabbet/Rabbet2.png" className=" w-full h-auto" />
+      </div>
       );
   }
 
   export function Characterdisplaythree() {
     return (
-      <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
-        <mesh>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
-      </Canvas>
+      // <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
+      //   <mesh>
+      //     <boxGeometry />
+      //     <meshNormalMaterial />
+      //   </mesh>
+      // </Canvas>
+      <div className=" lg:w-full md:w-full lg:max-h-[500px] lg:h-auto md:h-[350px] overflow-hidden ring-4 ring-emerald-400  rounded-xl">
+        <img src="/images/Characters/SupportingCharacter/village.png" className=" w-full h-auto" />
+      </div>
     );
 }
 
 export function Characterdisplayfour() {
   return (
-    <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </Canvas>
+    // <Canvas camera={{position:[2,2,2]}} style={{ width: '100%', height: '33rem' }}>
+    //   <mesh>
+    //     <boxGeometry />
+    //     <meshNormalMaterial />
+    //   </mesh>
+    // </Canvas>
+    <div className=" lg:w-full md:w-full lg:max-h-[500px] lg:h-auto md:h-[350px] overflow-hidden ring-4 ring-emerald-400  rounded-xl">
+        <img src="/images/Characters/SupportingRobot/robot.png" className=" w-full h-auto" />
+      </div>
   );
 }
 
