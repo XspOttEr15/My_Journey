c38da63e2b6c33b8bdff97e06f9b79d7fd209f8c
Natthapong Pecpipat <102534597+XspOttEr15@users.noreply.github.com>
2024-03-21 17:37:03 +0700

    add tutorial page 2

--- src/components/Canvas/Instructions.jsx
+++ src/components/Canvas/Instructions.jsx
@@ -5,12 +5,13 @@ const Instructions = ({ isVisible }) => {
   return (
     <div className={`instructions-overlay${isVisible ? ' visible' : ''}`}>
       <div className="overlay"></div>
-      <h1 id="button" >CLICK HERE TO PLAY</h1>
+      <div className='text-6xl mb-5'>Tutorial</div>
       <p className='text-xl'>Move: WASD</p>
       <p className='text-xl'>Jump: SPACEBAR</p>
       <p className='text-xl'>Look: MOUSE</p>
       <p className='text-xl'>Cursor: ESC</p>
       <p className='text-xl'>Navbar: ESCx2</p>
+      <button id="button"  type="button" class="mt-5 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200  font-medium rounded-lg  text-2xl px-5 py-2.5 text-center me-2 mb-2">CLICK HERE TO PLAY</button>
     </div>
   );
 };
